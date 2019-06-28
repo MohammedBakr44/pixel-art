e781e52c41beec4619de9a59609f748f3be528f5
MohammedBakr44 <mohammed.mobile.tricks@gmail.com>
2019-06-28 16:49:02 +0200

    Update designs.js

--- designs.js
+++ designs.js
@@ -2,47 +2,77 @@
 // Select size input
 
 // When size is submitted by the user, call makeGrid()
-
+const gridDimentions = 20;
+/*
+ * @param element to be selected
+ * TODO: return the selected element to use it(just like in JQuery)
+ */
 function $(element) {
     let el = document.querySelector(element);
     return el;
 }
 
-
-function makeGrid() {
-
-    $('#sb').onclick = function () {
-        console.log('hello')
-
-        let height = $('#inputHeight').value;
-        let width = $('#inputWidth').value;
-
-        let gridW = '';
-        for (let i = 0; i < width; i++) {
-            gridW += '<div class="grid-item"></div>';
-        }
-        let gridH = '';
-        for (let i = 0; i < height; i++) {
-            gridH += gridW
-        }
-        $('#pixelCanvas').innerHTML = gridH
-        $('#pixelCanvas').style.width = width * height + 'px'
-        const c = document.querySelectorAll('.grid-item');
-        const color = $('#colorPicker').value;
-        console.log(color);
-        for (var i = 0; i < c.length; i++) {
-            c[i].style.width = 100 / width + '%';
-            c[i].style.flex = 100 / width + '%';
-        }
-
-        for (let i = 0; i < document.querySelectorAll('.grid-item').length; i++) {
-            document.querySelectorAll('.grid-item')[i].onclick = function () {
+/*
+ * @param el: element to be clicked
+ * @param color: colorPicker which have the color value
+ * result: color the clicked element(pixel)
+ * result: Remove the colored pixel if it's clicked(undo)
+ */
+function colorPixels(el, color) {
+    for (let i = 0; i < el.length; i++) {
+        el[i].onclick = function() {
+            if (this.classList.contains('colored')) {
+                this.style.backgroundColor = 'transparent';
+                this.classList.remove('colored');
+            } else {
                 this.style.backgroundColor = color;
+                this.classList.add('colored');
             }
-        }
-
+        };
     }
-
 }
 
-makeGrid();
+
+
+function makeGrid() {
+    // Reset Grid
+    $('#pixelCanvas').innerHTML = '';
+
+    // Take inputs values
+    let height = $('#inputHeight').value;
+    let width = $('#inputWidth').value;
+
+    // Grid element
+    function createGridItem() {
+        const gElement = document.createElement('div');
+        gElement.classList.add('grid-item');
+        gElement.style.width = `${gridDimentions}px`;
+        gElement.style.height = `${gridDimentions}px`;
+        gElement.style.flex = 100 / width + '%';
+        return gElement;
+    }
+
+    // Canvas
+    $('#pixelCanvas').style.width = width * gridDimentions + 'px';
+    $('#pixelCanvas').style.height = height * gridDimentions + 'px';
+
+    for (let i = 0; i < width * height; i++) {
+        $('#pixelCanvas').appendChild(createGridItem());
+    }
+
+    colorPixels(
+        document.querySelectorAll('.grid-item'),
+        $('#colorPicker').value
+    );
+}
+
+$('#sb').onclick = function() {
+    makeGrid();
+};
+
+$('#colorPicker').onchange = function() {
+    colorPixels(
+        document.querySelectorAll('.grid-item'),
+        $('#colorPicker').value
+    );
+};
